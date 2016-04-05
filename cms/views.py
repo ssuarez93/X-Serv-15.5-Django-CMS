b327c066b6384077f94b7c40a3fbe0193636a3b0
Sara <sara_suasan_93@hotmail.com>
2016-04-05 21:23:04 +0200

    Entrega ejercicio 15.5 por Sara Suárez

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,22 @@
+# coding=utf-8
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
 # Create your views here.
+
+def identificador(request, ident):
+    try:
+        pages = Pages.objects.get(id=int(ident))
+        pagina = str(pages.page)
+    except Pages.DoesNotExist:
+        pagina = ("<h4>Error. No hay pagina para el identificador introducido</h3>")
+    return HttpResponse(pagina)
+
+def recurso(request, recurso):
+    try:
+        pages = Pages.objects.get(name=recurso)
+        pagina = str(pages.page)
+    except Pages.DoesNotExist:
+        pagina = ("<h4>Error. No hay pagina para el recurso introducido</h3>")
+    return HttpResponse(pagina)
